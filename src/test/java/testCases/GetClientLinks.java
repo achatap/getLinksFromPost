dc102aa5617192a84529061213c22530c5519548
Anurag Chatap <achatap@gmail.com>
2021-06-05 20:53:23 +0530

    added testng and pom dependencies

---
+++ src/test/java/testCases/GetClientLinks.java
@@ -0,0 +1,11 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+public class GetClientLinks {
+
+    @Test
+    public void extractLinks(){
+        System.out.println("Hi");
+    }
+}
